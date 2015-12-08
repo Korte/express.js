c6bff2ffe2dd04323977634998fdba45966f849f
Sonny Korte <sonny.korte@bestbrains.dk>
2015-12-08 23:25:08 +0100

    Load users.json and return it

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hi Sonny Korte')
+var fs = require('fs')
+var _ = require('lodash')
+
+var users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, function(err, data) {
+    if (err) throw err;
+
+    JSON.parse(data).forEach(function (user) {
+        user.name.full =  _.startCase(user.name.first + ' ' + user.name.last)
+        users.push(user)
+    })
 })
 
-app.get('/swap', function (req, res) {
-    res.send('Hi Korte Sonny')
+
+app.get('/', function (req, res) {
+    res.send(JSON.stringify(users, null, 2))
 })
+
 
 var server =  app.listen(3000, function(){
 
